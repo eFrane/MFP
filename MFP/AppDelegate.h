6dd122ccf35a4393d2a43825197a0ca1f77dc7aa
Stefan Graupner <stefan.graupner@gmail.com>
2013-02-26 10:44:50 +0100

    Initial Commit

---
+++ MFP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MFP
+//
+//  Created by Stefan Graupner on 26.02.13.
+//  Copyright (c) 2013 meanderingsoul.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MFP/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MFP
+//
+//  Created by Stefan Graupner on 26.02.13.
+//  Copyright (c) 2013 meanderingsoul.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
